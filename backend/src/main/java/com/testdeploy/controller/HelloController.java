4c7bbe7bb4514c9a864be9a14415bcbc323f09bb
清净 <qingjing666666@126.com>
2026-02-08 18:37:56 +0800

    deploy-b

--- backend/src/main/java/com/testdeploy/controller/HelloController.java
+++ backend/src/main/java/com/testdeploy/controller/HelloController.java
@@ -5,6 +5,9 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class HelloController {
 
@@ -18,8 +21,12 @@ public class HelloController {
         Long count = redisTemplate.opsForValue().increment(COUNT_KEY);
         return """
                 <h1> Hello World! </h1>
+                <p> 当前时间：%s </p>
                 <p> 这是第 %d 次访问 </p>
-                """.formatted(count);
+                """
+                .formatted(
+                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                        count);
     }
 
     @GetMapping("/count")
