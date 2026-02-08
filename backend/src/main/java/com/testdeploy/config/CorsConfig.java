a61a4489fcadecda13e24e23ba4d8198cbc2b55a
清净 <qingjing666666@126.com>
2026-02-08 15:39:11 +0800

    deploy-b

--- backend/src/main/java/com/testdeploy/config/CorsConfig.java
+++ backend/src/main/java/com/testdeploy/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 所有接口都支持跨域
-                .allowedOrigins("*") // 允许所有来源，生产环境应该限制具体域名
+                .allowedOriginPatterns("*") // 允许所有来源，生产环境应该限制具体域名
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许所有 HTTP 方法
                 .allowedHeaders("*") // 允许所有请求头
                 .allowCredentials(true) // 允许发送 Cookie
