8164bd30006a547dd90803b34f7c90fb2d547a57
清净 <qingjing666666@126.com>
2026-02-08 15:09:06 +0800

    deploy-f

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# test-pages
+# test-pages-deploy
 
 用于测试 Vue 3 项目使用 GitHub Actions 在 GitHub Pages 上的自动部署
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   ],
   // 关键：设置基础路径为你的仓库名
   base: process.env.NODE_ENV === 'production' 
-    ? '/test-pages/'  // 替换为你的实际仓库名
+    ? '/test-pages-deploy/'  // 替换为你的实际仓库名
     : '/',
   resolve: {
     alias: {
